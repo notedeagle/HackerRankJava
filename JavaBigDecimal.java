d397a4f16c418a4cac170770bc69c5dbffd4e2b8
notedeagle <notedeagle@gmail.com>
2021-02-23 11:31:25 +0100

    Add files via upload

---
+++ JavaBigDecimal.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        Comparator<String> myComparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal b1 = new BigDecimal(s1);
+                BigDecimal b2 = new BigDecimal(s2);
+                return b2.compareTo(b1); 
+            }
+        };
+        
+        Arrays.sort(s, 0, n, myComparator); 
+        
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
